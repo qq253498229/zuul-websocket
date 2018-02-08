4d7638f1d79e4f2b48def28303278cbb97e86a05
david wang <253498229@qq.com>
2018-02-09 02:49:33 +0800

    initial commit

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,113 +1,48 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li v-for="(item,idx) in list">{{item}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import SockJS from 'sockjs-client'
+
+  export default {
+    data() {
+      return {
+        msg: 'Test WebSocket',
+        websocket: {},
+        list: []
+      }
+    },
+    mounted() {
+      let that = this
+      this.$http.get('api/service-websocket/test').then(res => {
+        console.log(res)
+      })
+      if (window['WebSocket']) {
+        //原声websocket
+        // that.websocket = new WebSocket("ws://localhost:8081/websocket");
+        this.websocket = new WebSocket("ws://localhost:8080/api/service-websocket/websocket");
+      } else {
+        that.websocket = new SockJS("http://localhost:8080/api/service-websocket/websocket/socketjs");
+      }
+      that.websocket.onopen = function (event) {
+        console.log("websocket开启:" + event);
+      }
+      that.websocket.onclose = function (event) {
+        console.log("websocket关闭:" + event);
+      };
+      that.websocket.onmessage = function (event) {
+        console.log(event.data);
+        that.list.push(event.data)
+      }
+
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- service-websocket/src/main/java/cn/codeforfun/App.java
+++ service-websocket/src/main/java/cn/codeforfun/App.java
@@ -3,7 +3,10 @@ package cn.codeforfun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
@@ -11,7 +14,13 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  */
 @SpringCloudApplication
 @EnableScheduling
+@RestController
 public class App {
+  @GetMapping("/test")
+  public ResponseEntity test() {
+    return ResponseEntity.ok("test");
+  }
+
   @Bean
   public ServerEndpointExporter serverEndpointExporter() {
     return new ServerEndpointExporter();
